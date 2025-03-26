184dc5651176572c128e9c6e9a9c426f1c4f49f2
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-03-25 21:25:26 -0400

    Update info.js

--- structures/slashcommands/information/info.js
+++ structures/slashcommands/information/info.js
@@ -13,6 +13,6 @@ module.exports = {
 
     run: async (client, interaction) => {
         const user = interaction.user.id
-        return interaction.reply(`${user}!\n ${userAccount.getUserCache(user.userId)}`)
+        return interaction.reply(`${user}!\n ${userAccount.getUserCache(user)}`)
     }
 }
